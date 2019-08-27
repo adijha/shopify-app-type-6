3f5e96df3453b6b40c00676983342b40bb7e45f1
Aditya Kumar Jha <adityajha126@gmail.com>
2019-08-27 22:42:50 +0530

    -

---
+++ index.js
@@ -0,0 +1,58 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = new express();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce');
+const querystring = require('querystring');
+const request = require('request-promise');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'write_products';
+const forwardingAddress = "https://abc1c6fb.ngrok.io";
+
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop + '/admin/oauth/authorize?client_id=' + apiKey + '&scope=' + scopes + '&state=' + state + '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    const map = Object.assign({}, req.query);
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const generatedHash = crypto
+      .createHmac('sha256', apiSecret)
+      .update(message)
+      .digest('hex');
+
+    if (generatedHash !== hmac) {
+      return res.status(400).send('HMAC validation failed');
+    }
+    res.status(200).send('HMAC validated');
+  }
+  else {
+    res.status(400).send('Required Parameters missing')
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000');
+});
